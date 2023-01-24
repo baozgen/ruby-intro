cebe11a6a008bcff2dd2e10da0f0eeb769d2a8b7
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-01-24 22:24:40 +0000

    adds more information

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,29 @@
 
 # Hashes are lists of *key-value pairs*
 
+profile = {
+    "name" => "Ben",
+    "location" => "Evanston",
+    "status" => "Teaching",
+    "timeline" => [
+        {"status" => "Teaching", "time" => "3:58 pm"},
+        {"status" => "Driving to class", "time" => "12:50 pm"}
+    ]
+}
+# => is hashrocket
+# different types of data... it's not an array
+puts profile
+
 # Accessing data from the hash
+puts profile["name"]
+name = profile["name"]
+puts name
+#ruby is case sensitive
+
+puts profile["timeline"][0]["status"]
+
 
 # More Complex Hashes
+profile["name"] = "Benjamin"
+profile["age"] = "..."
+puts profile.keys
