cc38cd1a3ee63392d70af7298ce794732bba798b
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-01-24 20:18:11 +0000

    practices ruby

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,56 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+# "outputs"...
+puts 2
+puts 5 + 2
+puts 5 * 2
+puts 5 / 2
+# when we use integer, ruby gives us back an integer
+puts 5 % 2
+
+puts 5.0 / 2.0
+# use float to get a float
+
+puts 5.0 / 2
+# ruby predicts the data type
+
+puts 5.0 * 2 + 1
+
 
 # Perform simple math with numbers
+puts 5.0 * (2 + 1)
+puts 5.0 * (2 + 1)
 
 # Strings
+puts "Hello, world!" 
 
 # Combine strings together
+puts "Tacos are " + "delish"
+puts "Tacos" * 3
+# puts 3 * "Tacos" 
+    # does not work
+puts "Tacos: " + 3.to_s
+#converts to string
 
 # Variables
+food = "Tacos"
+quantity = 3
+puts food * quantity
 
 # Combine strings and variables
+first_name = "Grogu"
+puts "Hello, " + first_name + "!"
+puts "Hello, #{first_name}!"
+#string interpolation
+
+greeting = "Hello, #{first_name}!"
+puts greeting
+
+puts "#{food}: #{quantity}"
 
 # String manipulation
+puts first_name.upcase
+puts greeting.reverse
+puts greeting.swapcase
